e99202eaa5a2c6a7bcef198afabb3541e1e8bec6
Steffen Moeller <moeller@debian.org>
2017-05-01 13:20:32 +0200

    Reasonably lintian clean

--- debian/extra/einstein_bench/runall.sh
+++ debian/extra/einstein_bench/runall.sh
@@ -1,9 +1,22 @@
+#!/bin/sh
+
+set -e
+
+BENCHMARKSCRIPT=$(dirname $0)/bench_single.sh 
+
+if [ ! -x "$BENCHMARKSCRIPT" ]; then
+	echo "E: Could not find benchmark script on '$BENCHMARKSCRIPT'."
+	exit 1
+fi
+
 for APP in `ls apps | grep -v archive`; do
-	echo "Starting test with $APP";
-	./bench_single.sh ${APP} &
+	echo "I: Starting test with $APP";
+	$BENCHMARKSCRIPT ${APP} &
 done
+
 # wait for boinc_EinsteinRadio_0 creation
 sleep 30;
+
 while true; do
 	for APP in `ls apps | grep -v archive`; do
 		awk -F"<|>" '/fraction_done/ { print $3 }' ${APP}/boinc_EinsteinRadio_0 ;
