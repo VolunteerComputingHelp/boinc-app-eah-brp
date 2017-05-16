36adb320d6d87e40b2e7a77c5f9a311d937f694f
Steffen Moeller <moeller@debian.org>
2017-05-16 11:48:55 +0200

    Architecture-dependent wisdom generation

--- debian/extra/create_wisdomf_eah_brp.sh
+++ debian/extra/create_wisdomf_eah_brp.sh
@@ -9,18 +9,37 @@ set -e
 
 if [ "-h" = "$1" -o "--help" = "$1" ]; then
 cat <<EOHELP
-Usage: $(basename $0) -h|--help - show this help
-       $(basename $0) [file]    - compute and write wisdomf to file
+Usage:  $(basename $0) -h|--help - show this help
+        $(basename $0) [file]    - compute and write wisdomf to file
 
-The FFWT library offers the concept of wisdom files to cache the best
-solutions for dissecting large series of data into serial invocations
-of FFWT algorithmic variants.
+DESCRIPTION
+        The FFWT library offers the concept of wisdom files to cache
+        the best solutions for dissecting large series of data into
+        serial invocations of FFWT algorithmic variants.
 
-This script is provided by the Einstein@Home community to support the
-efficiency of the search for Binary Radio Pulsars.  On a lower level,
-the wisdom file itself is created by the fftwf-wisdom tool as provided
-by the ffwt library which takes between 6 and 120 hours to compute,
-depending on the platform it is executed on.
+        This script is provided by the Einstein@Home community to
+        support the efficiency of the search for Binary Radio Pulsars
+        (BRP) and for sufficiently capable hardware also the Gamma-ray
+        pulsar binary search (FGRP).  On a lower level, the wisdom file
+        itself is created by the fftwf-wisdom tool as provided by the
+        ffwt library which takes between 6 and 120 hours to compute,
+        depending on the platform it is executed on.
+
+ENV
+        WISDOMFLAGS	If the '-n' or '-c' oprion shall be defined,
+                        set the WISDOMFLAGS.
+
+EXAMPLE
+        sudo $(basename $0) /etc/fftw/wisdomf
+	
+        WISDOMFLAGS="-c" $(basename $0)
+
+AUTHORS
+        Steffen Moeller <moeller@debian.org>
+        Christian Dreihsig <christiandreihsig@t-online.de>
+
+SEE ALSO
+	https://github.com/VolunteerComputingHelp/boinc-app-eah-brp/blob/master/debian/extra/create_wisdomf_eah_brp.sh
 
 EOHELP
 exit
@@ -45,7 +64,32 @@ if [ -r /etc/fftw/wisdomf ]; then
 	echo
 fi
 
-fftwf-wisdom -v -o "$DESTFILE" rof12582912 rob67108864
+ARCH=$(arch)
+if [ -z "$ARCH" ]; then
+	echo "E: Cannot tell the platform you are working with, do you have the 'arch' tool installed?"
+	exit -1
+fi
+
+
+echo "I: Generating wisdom for 2^24 bit projects (BRP)"
+TAGS=rof12582912
+
+if echo "$ARCH" | egrep -q 'i386|x86_64|powerpc'; then
+	echo "I: Also generating wisdom for 2^26 bit projects (FGRP)"
+	TAGS="$TAGS rob67108864"
+
+else
+	# some extra checks on CPU speeds should go here
+	# pointless as of today since no other client than BRP are available for this platform
+	echo "W: Not recognising platform '$ARCH' as sufficiently capable for 2^26 bit projects"
+fi
+
+if [ -n "$WISDOMFLAGS" ]; then
+	echo "I: Adding flags '$WISDOMFLAGS' as defined by envvar WISDOMFLAGS"
+fi
+
+fftwf-wisdom $WISDOMFLAGS -v -o "$DESTFILE" $TAGS	# do not use quotes around $TAGS or $WISDOMFLAGS
+
 # -n asks to ignore earlier wisdomf files (not set)
 # -c adds a collection of additional plans (not set)
 # rof12582912 supports BRP, identified by N30dG
